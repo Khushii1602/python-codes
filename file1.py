91fdad7709ddb764ba6cc0286fb6188fb9300fa3
Khushii1602 <j.khushi1.6.0.4@gmail.com>
2024-06-17 20:24:53 +0530

    first commit

---
+++ file1.py
@@ -0,0 +1,6 @@
+import math
+import keyword
+num1 = int(input("Enter 1st number"))
+num2 = int(input("Enter 2nd number"))
+sum = int((num1 + num2))
+print("sum of ", num1 ,"and", num2 , "is :" ,sum)
