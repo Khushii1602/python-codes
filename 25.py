2671d7bba6619e923525f13d3ac7acb658936e81
Khushii1602 <j.khushi1.6.0.4@gmail.com>
2024-06-17 22:22:57 +0530

    19

---
+++ 25.py
@@ -0,0 +1,7 @@
+with open('first.txt','r') as firstfile, open('second.txt','w') as secondfile: 
+      
+    # read content from first file 
+    for line in firstfile: 
+               
+             # write content to second file 
+             secondfile.write(line)
