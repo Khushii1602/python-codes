3135101d9ef6fcf58815eff2be7a31a5a4378323
Khushii1602 <j.khushi1.6.0.4@gmail.com>
2024-06-18 18:26:02 +0530

    commit

---
+++ path.py
@@ -0,0 +1,6 @@
+with open("C:/Users/KHUSHI/textfile.txt", 'r') as file:content = file.read()
+print(content)
+
+'''
+with open("C:/Users/Satakshi Wadhwa/OneDrive/Desktop/text2.txt",'r') as file:content = file.read()
+print(content)'''
